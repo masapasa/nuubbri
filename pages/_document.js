eb6b830d2669f7d684f34975c278aa48233a95e3
aswin <apyakurel@gmail.com>
2020-06-12 16:28:57 +0200

    amzn affiliate

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=PT+Sans:300,400,700,800"
             rel="stylesheet"
           />
+          <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=nubri-20&marketplace=amazon&region=US&placement=B083GH7LXW&asins=B083GH7LXW&linkId=9022535ea3e73ba240b39596fd891e9d&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066C0&bg_color=FFFFFF">
+    </iframe>
           <script data-ad-client="ca-pub-5827971423225899" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
---
+++ utils/db.js
@@ -0,0 +1,1 @@
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
